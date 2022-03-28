860191a031aae4bdca446d6573b7a4224e4fd5ed
Faustino Villasboa Martínez <fvillasboa96@gmail.com>
2022-03-27 21:04:33 -0400

    Controlador Añadido

---
+++ database/migrations/2022_03_27_193906_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**Clase que crea una migracion para una tabla determinada
+ * Creada con el comando php artisan make:migration nombremigracion(CreateNameTable)*/
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description', 1000);
+            $table->float('price')->unsigned();
+            $table->integer('stock')->unsigned();
+            $table->string('status')->default('unavailable');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
